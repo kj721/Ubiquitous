b6be3c4542bec335b7bcfac6d6648ab970078c73
kj721 <38840376+kj721@users.noreply.github.com>
2024-09-18 12:04:01 -0700

    variables

---
+++ Tools/variables.jl
@@ -0,0 +1,43 @@
+x = 42
+
+x + 12
+
+x = 12; y = 15; z = 18;
+
+y = x^3 + 3*x
+
+x = "Programming in Julia"
+
+z = 3.14
+typeof(z)
+z = "this is a string"
+typeof(z)
+
+_abc = 213
+println(_abc)
+
+α = 15 # \alpha TAB
+α
+
+β = 23 # \beta TAB
+β
+
+α * β
+
+β₀ = 3 # \beta TAB \_0 TAB
+β₁ = 4 
+β₀ * β₁
+
+α¹ = 13 # \alpha TAB \^1 TAB
+
+π # \pi TAB
+# π = 45 you cannot assign a new value to println
+
+ℯ # \euler TAB
+
+# this is a one-line comment
+
+#= This is a
+multi-line 
+comment =#
+
