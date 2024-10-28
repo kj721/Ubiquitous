8cdd67b513f32d3bd7378ebeff45898175f598a8
kj721 <38840376+kj721@users.noreply.github.com>
2024-10-27 22:48:56 -0700

    julia variables

---
+++ Tools/variables_2.jl
@@ -0,0 +1,28 @@
+x = 42
+
+x + 23
+
+x = 12; y = 15; z = 18
+
+y = x^3 + 3*x
+
+x = "Programming in Julia"
+
+x
+
+z = 3.14
+typeof(z)
+
+z = "This is a string"
+
+typeof(z)
+
+_abc = 213
+
+println(_abc)
+
+α = 15
+println(α)
+
+β = 234
+
